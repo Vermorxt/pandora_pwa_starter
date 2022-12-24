187206c449d1924f13578ea818398bf39058d222
MakiMarc <marc@keleya.de>
2022-12-25 00:41:46 +0100

    added language support again

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-})
+
 const { i18n } = require('./next-i18next.config')
 
-const dev = process.env.ENV !== 'production'
-
-console.log('----> process env: ', process.env)
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    skipWaiting: true,
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
   },
   i18n,
+}
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  disable: false,
+  register: true,
 })
+
+module.exports = withPWA(nextConfig)
