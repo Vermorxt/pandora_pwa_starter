4c91dd38d6e62f887a0799bd0c4c1de2a050dcda
MakiMarc <marc@keleya.de>
2022-12-29 16:05:16 +0100

    bluetooth works

--- pages/dashboard/bluetooth/index.tsx
+++ pages/dashboard/bluetooth/index.tsx
@@ -5,6 +5,12 @@ import { useEffect, useState } from 'react'
 import { AnyType } from '../../../src/_types/anytype'
 
 const HEART_RATE_SERVICE = '0000180d-0000-1000-8000-00805f9b34fb'
+const HEART_RATE_MEASUREMENT_CHARACTERISTIC = '00002a37-0000-1000-8000-00805f9b34fb'
+const BODY_SENSOR_LOCATION_CHARACTERISTIC = '00002a38-0000-1000-8000-00805f9b34fb'
+const BATTERY_SERVICE = numberToUUID(0x180f)
+const BATTERY_CHARACTERISTIC = numberToUUID(0x2a19)
+const POLAR_PMD_SERVICE = 'fb005c80-02e7-f387-1cad-8acd2d8df0c8'
+const POLAR_PMD_CONTROL_POINT = 'fb005c81-02e7-f387-1cad-8acd2d8df0c8'
 
 const Bluetooth = () => {
   const [btError, setBtError] = useState<string>()
@@ -13,67 +19,79 @@ const Bluetooth = () => {
 
   const initBluetooth = async () => {
     try {
-      await BleClient.initialize({ androidNeverForLocation: true })
-    } catch (e) {
-      const err = e as AnyType
-
-      setBtError(err.toString() as string)
+      await BleClient.initialize()
+    } catch (error) {
+      console.error(error)
     }
   }
 
   const startScan = async () => {
-    const state = await BleClient.isEnabled()
+    // const state = await BleClient.isEnabled()
 
-    setScanStatus(`Starte scan ... ${state as unknown as string}`)
+    // setScanStatus(`Starte scan ... ${state as unknown as string}`)
 
-    await BleClient.requestLEScan({}, result => {
-      console.log('received new scan result', result)
-      setScanStatus('Devices found ...')
-      setDevices([...devices, result])
-    })
+    // await BleClient.requestLEScan({}, result => {
+    //   console.log('received new scan result', result)
+    //   setScanStatus('Devices found ...')
+    //   setDevices([...devices, result])
+    // }).catch(error => {
+    //   setScanStatus('Someting went wrong ...')
+    //   console.log('error connection')
+    // })
 
-    // await BleClient.requestLEScan(
-    //   {
-    //     allowDuplicates: false,
-    //     scanMode: 1,
-    //   },
-    //   result => {
-    //     console.log('received new scan result', result)
-
-    //     setScanStatus('Devices found ...')
-
-    //     devices.push(result)
-
-    //     setDevices([...devices])
-    //   }
-    // )
-
-    navigator.bluetooth.addEventListener('advertisementreceived', event => {
-      setScanStatus('Devices found event ...')
-
-      console.log('Advertisement received.')
-      console.log('  Device Name: ', event.device.name)
-      console.log('  Device ID: ', event.device.id)
-      console.log('  RSSI: ', event.rssi)
-      console.log('  TX Power: ', event.txPower)
-      console.log('  UUIDs: ', event.uuids)
-      event.manufacturerData.forEach((valueDataView, key) => {
-        console.log('Manufacturer', key, valueDataView)
-      })
-      event.serviceData.forEach((valueDataView, key) => {
-        console.log('Service', key, valueDataView)
+    try {
+      const device = await BleClient.requestDevice({
+        // services: [HEART_RATE_SERVICE],
+        // optionalServices: [BATTERY_SERVICE, POLAR_PMD_SERVICE],
       })
 
-      devices.push(event.device as unknown as ScanResult)
-      setDevices([...devices])
-    })
+      // connect to device, the onDisconnect callback is optional
+      await BleClient.connect(device.deviceId, deviceId => onDisconnect(deviceId))
+      console.log('connected to device', device)
 
-    setTimeout(() => {
-      void BleClient.stopLEScan()
-      setScanStatus('Stopped scan ...')
+      const result = await BleClient.read(device.deviceId, HEART_RATE_SERVICE, BODY_SENSOR_LOCATION_CHARACTERISTIC)
+      console.log('body sensor location', result.getUint8(0))
 
-      console.log('stopped scanning')
-    }, 35000)
+      const battery = await BleClient.read(device.deviceId, BATTERY_SERVICE, BATTERY_CHARACTERISTIC)
+      console.log('battery level', battery.getUint8(0))
+
+      await BleClient.write(device.deviceId, POLAR_PMD_SERVICE, POLAR_PMD_CONTROL_POINT, numbersToDataView([1, 0]))
+      console.log('written [1, 0] to control point')
+
+      await BleClient.startNotifications(
+        device.deviceId,
+        HEART_RATE_SERVICE,
+        HEART_RATE_MEASUREMENT_CHARACTERISTIC,
+        value => {
+          console.log('current heart rate', parseHeartRate(value))
+        }
+      )
+
+      // disconnect after 10 sec
+      setTimeout(() => {
+        void BleClient.stopNotifications(device.deviceId, HEART_RATE_SERVICE, HEART_RATE_MEASUREMENT_CHARACTERISTIC)
+        void BleClient.disconnect(device.deviceId)
+        console.log('disconnected from device', device)
+      }, 10000)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  function onDisconnect(deviceId: string): void {
+    console.log(`device ${deviceId} disconnected`)
+  }
+
+  function parseHeartRate(value: DataView): number {
+    const flags = value.getUint8(0)
+    const rate16Bits = flags & 0x1
+    let heartRate: number
+    if (rate16Bits > 0) {
+      heartRate = value.getUint16(1, true)
+    } else {
+      heartRate = value.getUint8(1)
+    }
+    return heartRate
   }
 
   useEffect(() => {
