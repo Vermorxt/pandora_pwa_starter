48f4a27e431e47d6811adf5f0f2a6bb5a8415a68
MakiMarc <marc@keleya.de>
2023-01-06 00:19:46 +0100

    adding households

--- pages/dashboard/households/index.tsx
+++ pages/dashboard/households/index.tsx
@@ -1,15 +1,25 @@
-import { Ui_Button, Ui_Modal } from '@vermorxt/pandora_ui'
+import { Ui_Button, Ui_Modal, Ui_Stat } from '@vermorxt/pandora_ui'
 import { Helper } from '@vermorxt/pandora_utils'
+import dayjs from 'dayjs'
 import React, { useState } from 'react'
 import { formIsValid, useSubmit } from '../../../src/modules/form'
 import { Ui_Form } from '../../../src/modules/form/hooks/form-context'
 import { InitialFormValues } from '../../../src/modules/form/_types/form/initial-form-values'
+
+export interface HouseHold {
+  id?: string
+  name?: string
+  address?: string
+  active?: boolean
+}
 
 const HouseholdsPage = () => {
   const [showError, setShowError] = useState(false)
   const [loginFailed, setLoginFailed] = useState(false)
   const [disabled, setDisabled] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [forceClose, setForceClose] = useState<boolean>()
+  const [houseHold, setHouseHold] = useState<HouseHold[]>([])
 
   const formInitialValues = [
     {
@@ -42,24 +52,49 @@ const HouseholdsPage = () => {
 
       console.log('add haushalt error')
 
-      setTimeout(() => {
-        const errorFields = document.querySelectorAll("[type='error']") as unknown as NodeListOf<Element>
-        if (errorFields) Helper.scrollToExistingElement(errorFields[0])
-      }, 300)
-
       return
     }
 
     setLoading(false)
     console.log('add haushalt')
+
+    const house = {
+      name: values?.name,
+      address: values?.address,
+    }
+
+    houseHold.push(house)
+    setHouseHold([...houseHold])
+    setForceClose(true)
+
+    setTimeout(() => {
+      setForceClose(undefined)
+    }, 1000)
   })
 
   return (
     <div>
       <h1>Haushalte</h1>
-      <div style={{ marginTop: 20 }}>
+      <div style={{ marginTop: 20, marginBottom: 40 }}>
+        <div style={{ marginBottom: 40 }}>
+          {houseHold.length > 0 &&
+            houseHold.map((house, index) => (
+              <Ui_Stat key={index} style={{ width: '100%', marginBottom: 20 }}>
+                <Ui_Stat.Item>
+                  <Ui_Stat.Title>{house?.address}</Ui_Stat.Title>
+                  <Ui_Stat.Value>{house?.name}</Ui_Stat.Value>
+                  <Ui_Stat.Description>Ersttellt: {dayjs().format('DD.MM.YYYY HH:mm')} </Ui_Stat.Description>
+                </Ui_Stat.Item>
+                <Ui_Stat.Item className="flex justify-center items-center">
+                  <Ui_Button size="small">Verwalten</Ui_Button>
+                </Ui_Stat.Item>
+              </Ui_Stat>
+            ))}
+        </div>
+
         <Ui_Modal
           closeClickOutside={false}
+          forceClose={forceClose}
           id="modal_1"
           size="large"
           buttonText="Haushalt hinzufügen"
