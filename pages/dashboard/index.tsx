857bfd85bf3a9aa253d977772c712e4dbb2b2d47
MakiMarc <marc@keleya.de>
2023-01-14 13:56:48 +0100

    added dummy type module types

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,3 +1,13 @@
-const Dashboard = () => <h1 className="text-3xl font-bold">DASHBOARD</h1>
+const Dashboard = () => {
+  const handleChange = (value: any) => {
+    console.log('handle change: ', value)
+  }
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">DASHBOARD</h1>
+    </>
+  )
+}
 
 export default Dashboard
